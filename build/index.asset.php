b8476d43563e68665b92ba05c2c5f174c848effa
pavelzolotin <pavel.zolotin@goat.digital>
2022-04-28 13:45:03 +0500

    message

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '955592e05e63e35ae35dbbd7c8956312');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '5f939a9215cce9bc17c062f3836a330f');
